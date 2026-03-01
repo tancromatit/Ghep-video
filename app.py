7f28a006aaeb52cfef34e04ad16d952017d6335d
tancromatit <110036988+tancromatit@users.noreply.github.com>
2026-03-01 23:15:34 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -33,4 +33,8 @@ def upload():
 
 
 if __name__ == "__main__":
-    app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
